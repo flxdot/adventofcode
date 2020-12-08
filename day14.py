ac091bdd2ba3c2840c437fbcc2d3a6623953e2a7
Felix Fanghaenel <Felixfang@gmx.de>
2020-12-08 07:36:17 +0100

    updated skeletons

--- day14.py
+++ day14.py
@@ -8,6 +8,7 @@ from common import read_input
 
 convert_output = str
 
+
 def input_converter(input_line: str) -> convert_output:
     return str(input_line)
 
@@ -22,7 +23,6 @@ def solve_day14_part2(converted_input: List[convert_output]):
 
 if __name__ == "__main__":
     raw_input = read_input("inputs/day14.txt", input_converter)
-    
+
     print(f"Solution of Day 1 - Part 1 is '{solve_day14_part1(raw_input)}'")
     print(f"Solution of Day 1 - Part 2 is '{solve_day14_part2(raw_input)}'")
-
