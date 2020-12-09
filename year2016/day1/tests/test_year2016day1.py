b38549be94ef1764ef09b9995d6a1a8780e6016b
Felix Fanghaenel <Felixfang@gmx.de>
2020-12-09 21:52:02 +0100

    y2016 d1 p1 - input is wrong but tests are working...

--- year2016/day1/tests/test_year2016day1.py
+++ year2016/day1/tests/test_year2016day1.py
@@ -5,11 +5,16 @@ from common import read_input
 from year2016.day1.year2016day1 import solve_part1, solve_part2, input_converter
 
 
-def test_solve_part1():
-    test_input = read_input(
-        join(relpath(dirname(__file__)), "test_input.txt"), input_converter
-    )
-    assert 1 == solve_part1(test_input)
+@pytest.mark.parametrize(
+    "test_input, expected",
+    [
+        ("R2, L3", 5),
+        ("R2, R2, R2", 2),
+        ("R5, L5, R5, R3", 12)
+    ]
+)
+def test_solve_part1(test_input, expected):
+    assert solve_part1([input_converter(test_input)]) == expected
 
 
 def test_solve_part2():
--- year2016/day1/year2016day1.py
+++ year2016/day1/year2016day1.py
@@ -1,17 +1,60 @@
+import math
 from os.path import join, relpath, dirname
-from typing import List
+from typing import List, Tuple
 from common import read_input
 
 
-convert_output = str
+convert_output = List[str]
+
+class GridWalker:
+
+    def __init__(self):
+
+        self._current_pos = [0, 0]
+        self._heading = math.pi/2
+
+    @property
+    def position(self):
+        return self._current_pos
+
+    @property
+    def distance_from_home(self):
+        return int(abs(self._current_pos[0]) + abs(self._current_pos[1]))
+
+    def __repr__(self):
+        return f"Self @ {self.position[0]},{self.position[1]} - {self.distance_from_home}"
+
+    def walk(self, direction: str):
+
+        heading, distance = self._convert_direction(direction)
+
+        self._heading += heading
+
+        self._current_pos[0] += int(math.cos(self._heading) * distance)
+        self._current_pos[1] += int(math.sin(self._heading) * distance)
+
+    @staticmethod
+    def _convert_direction(direction: str) -> Tuple[float, int]:
+        """Converts a direction into a heading and distance."""
+
+        direction_to_heading = {
+            "L": math.pi/2,
+            "R": -math.pi/2
+        }
+
+        return direction_to_heading[direction[0]], int(direction[1])
 
 
 def input_converter(input_line: str) -> convert_output:
-    return str(input_line)
+    return input_line.split(", ")
 
 
 def solve_part1(converted_input: List[convert_output]):
-    return 1
+
+    walker = GridWalker()
+    for direction in converted_input[0]:
+        walker.walk(direction)
+    return walker.distance_from_home
 
 
 def solve_part2(converted_input: List[convert_output]):
