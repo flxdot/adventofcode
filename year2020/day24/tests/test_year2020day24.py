1a4fe980218c7d242265350b6c9c14f118f272c9
Felix Fanghaenel <Felixfang@gmx.de>
2020-12-24 12:57:15 +0100

    y2020 d24 p1

--- year2020/day24/tests/test_year2020day24.py
+++ year2020/day24/tests/test_year2020day24.py
@@ -2,12 +2,23 @@ from os.path import join, relpath, dirname
 import pytest
 
 from common import read_input
-from year2020.day24.year2020day24 import solve_part1, solve_part2, input_converter
+from year2020.day24.year2020day24 import solve_part1, solve_part2, input_converter, execute_moves
+
+
+@pytest.mark.parametrize(
+    "directions,position",
+    [
+        ("esew", (0, -1, 1)),
+        ("esenee", (3, -3, 0))
+    ]
+)
+def test_execute_moves(directions, position):
+    assert position == execute_moves(input_converter(directions))
 
 
 def test_solve_part1():
     test_input = read_input(join(relpath(dirname(__file__)), "test_input.txt"), input_converter)
-    assert 1 == solve_part1(test_input)
+    assert 10 == solve_part1(test_input)
 
 
 def test_solve_part2():
--- year2020/day24/year2020day24.py
+++ year2020/day24/year2020day24.py
@@ -1,17 +1,53 @@
 from os.path import join, relpath, dirname
-from typing import List
+from typing import List, Tuple
 from common import read_input
 
 
-convert_output = str
+convert_output = List[Tuple[int, int, int]]
 
 
 def input_converter(input_line: str) -> convert_output:
-    return str(input_line)
+    direction_to_moves = {
+        'ne': (1, 0, -1),
+        'e': (1, -1, 0),
+        'se': (0, -1, 1),
+        'nw': (0, 1, -1),
+        'sw': (-1, 0, 1),
+        'w': (-1, 1, 0),
+    }
+
+    moves = []
+    while len(input_line) > 0:
+        try:
+            moves.append(direction_to_moves[input_line[:1]])
+            input_line = input_line[1:]
+        except KeyError:
+            moves.append(direction_to_moves[input_line[:2]])
+            input_line = input_line[2:]
+
+    return moves
+
+
+def execute_moves(moves: List[Tuple[int, int, int]]) -> Tuple[int, int, int]:
+
+    position = (0, 0, 0)
+    for move in moves:
+        position = (
+            position[0] + move[0],
+            position[1] + move[1],
+            position[2] + move[2],
+        )
+    return position
 
 
 def solve_part1(converted_input: List[convert_output]):
-    return 1
+
+    title_map = {}
+    for moves in converted_input:
+        title_pos = execute_moves(moves)
+        title_map[title_pos] = not title_map.get(title_pos, False)
+
+    return sum([1 for is_black in title_map.values() if is_black])
 
 
 def solve_part2(converted_input: List[convert_output]):
